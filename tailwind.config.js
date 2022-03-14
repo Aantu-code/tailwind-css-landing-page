c153961d518278041878cd0880cdaa96345b9369
Antu-code <jibonoshomapto3@gmail.com>
2022-03-13 21:18:42 -0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["public/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize:{
+          
+      },
+      colors:{
+         
+      },
+      fontFamily:{
+        body:['Nunito']
+      }
+ 
+    }
+   
+    
+  },
+  plugins: [],
+}
